f79a7fdaf2793b846057c6ef83ec04b8ca23b3c0
Jorge Ossa <jorgehossa@gmail.com>
2020-07-09 00:29:40 -0500

    Create routes and linked to entry point

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import Header from '../templates/Header';
+import Home from '../pages/Home';
+import Character from '../pages/Character';
+import Error404 from '../pages/Error404';
+
+// Define routes
+const routes = {
+  '/': Home,
+  '/:id': Character,
+  '/contact': 'Contanct',
+}
+
+// Define routes handler
+const router = async () => {
+  const header = null || document.getElementById('header');
+  const content = null || document.getElementById('content');
+
+  header.innerHTML = await Header();
+}
+
+export default router;
