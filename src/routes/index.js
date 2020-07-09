65ae663df3a2919f17e2257582839ec4ea9d8044
Jorge Ossa <jorgehossa@gmail.com>
2020-07-09 00:44:20 -0500

    Create getHash and resolveRoutes utils

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ import Header from '../templates/Header';
 import Home from '../pages/Home';
 import Character from '../pages/Character';
 import Error404 from '../pages/Error404';
+import getHash from '../utils/getHash';
+import resolveRoutes from '../utils/resolveRoutes';
 
 // Define routes
 const routes = {
